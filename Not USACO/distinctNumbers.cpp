07be128e816fa5bb627c87908d082fa209ccd659
jzpan1 <panzhenyu2014@gmail.com>
2022-10-07 15:28:38 -0500

    distinctNumbers

--- Not USACO/distinctNumbers.cpp
+++ Not USACO/distinctNumbers.cpp
@@ -1,1 +1,20 @@
 //https://cses.fi/problemset/task/1621
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+	int n, x, y;
+	unordered_set<int> nums(0);
+	cin >> n;
+	for (int i=0; i<n; i++)
+	{
+		cin >> x;
+		nums.insert(x);
+	}
+	cout << nums.size();
+	return 0;
+}
